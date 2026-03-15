025497138d6f10fdf2582b5c46fc191e5a4f5f56
Erwan Leboucher <erwanleboucher@gmail.com>
2026-03-15 16:10:42 +0100

    feat: watch blueprint.yaml for update and auto resend
    Add a file watcher that re-pushes the blueprint to the server whenever
    the blueprint file is modified while Newt is running, matching the
    reactive behavior of the Docker event monitor.

--- main.go
+++ main.go
@@ -1627,6 +1627,12 @@ persistent_keepalive_interval=5`, util.FixKey(privateKey.String()), util.FixKey(
 		}
 	}
 
+	if blueprintFile != "" {
+		go watchBlueprintFile(ctx, blueprintFile, func() error {
+			return sendBlueprint(client)
+		})
+	}
+
 	// Wait for context cancellation (from signal or service stop)
 	<-ctx.Done()
 
